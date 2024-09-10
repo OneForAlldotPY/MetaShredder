a9a594a4336fa570d315ef44f32d19bce2d4b685
OneForAll <vllds@pm.me>
2024-09-10 14:29:50 +0200

    Add the file wipe logic alongside with its data overwritting logic in 3 passes

--- wipper.py
+++ wipper.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-
+import random
 
 #Base pattern for the passes and overwritting
 def overwrite_pattern(file_path, pattern, passes):
@@ -15,3 +15,20 @@ def overwrite_pattern(file_path, pattern, passes):
 
     except Exception as e:
         print(f"Error overwriting file {file_path}: {e}")
+
+
+def wipe_file(file_path, passes=3):
+    patterns = [
+        bytearray([0] * os.path.getsiz(file_path)),
+        bytearray([255] * os.path.getsize(file_path)),
+        bytearray(random.getrandbits(8) for i in range(os.path.getsize(file_path)))
+    ]
+
+    for pattern in patterns[:passes]:
+        overwrite_pattern(file_path, pattern, 1)
+
+    try:
+        os.remove(file_path)
+    except Exception as e:
+        print(f"Error deleting file {file_path}: {e}")
+        
