206290ce752b59abda586ce4f7035399cdfd3c81
OneForAll <vllds@pm.me>
2024-09-17 19:29:40 +0200

    Change interactive/deletion confirmation logic and add free space wipe functionality
    - Added interactive confirmation before file and directory wiping operations.
    - Implemented free space wipe logic for both Windows and Unix-based systems to ensure no residual data remains.

--- main.py
+++ main.py
@@ -3,24 +3,32 @@ import os
 import argparse
 import directory_wipe
 import file_wipe
+import free_space_wipe
 
 #Main function
 def deletetion_confirmation():
     # Prompt the user for a confirmation before proceeding with the wiping process
-
-    response = input("This action will permanently delete files and directories on this path. Are you sure you want to proceed? (yes/no):").lower()
-    if response not in ("yes", "y"):
-        print("Operation cancelled.")
-        exit
+    
+    while True:
+        response = input("This action will permanently delete files and directories on this path. Are you sure you want to proceed? (yes/no): ").strip().lower()
+        if response in ("yes", "y"):
+            return True
+        elif response in ("no", "n"):
+            print("Operation cancelled.")
+            exit()
+        else:
+            print("Invalid input. Please enter 'yes' or 'no'.")
 
 def main():
     parser = argparse.ArgumentParser(description="Securely wipe files and directories.")
     parser.add_argument("path", help="Path to the file or directory to wipe.")
     parser.add_argument("--passes", type=int, default=3, help="Number of passes for overwriting files")
+    parser.add_argument("--wipe-free-space", action='store_true', help="Wipe the free space on the given path/drive")
 
     args = parser.parse_args()
 
     deletetion_confirmation()
+
 
     path = args.path
     passes = args.passes
@@ -31,6 +39,10 @@ def main():
         directory_wipe.wipe_directory(path, passes)
     else: 
         print(f"Error: {path} is neither a file or a directory.")
+        return
+    
+    if args.wipe_free_space:
+        free_space_wipe.wipe_free_space(path)
 
 if __name__ == "__main__":
     main()
