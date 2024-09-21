a8f66e90a2c29fb8c11adbc95dd587470901e135
OneForAll <vllds@pm.me>
2024-09-21 20:21:06 +0200

    Implement a Summary Report logic for the program

--- reporting.py
+++ reporting.py
@@ -20,3 +20,39 @@ def real_time_output(operation_type, path, passes=0):
         print(f"[{timestamp}] Unknown operation on: {path}")
 
 # Summary Reports
+
+
+class SummaryReport:
+    # Group of functions that make possible track the files, directories and errors retrieved by the program 
+    def __init__(self):
+        self.wiped_files = []
+        self.wiped_directories = []
+        self.errors = []
+    
+    def track_file_wipe(self, file_path):
+        self.wiped_files.append(file_path)
+
+    def track_directory_wipe(self, directory_path):
+        self.wiped_directories.append(directory_path)
+
+    def track_error(self, error_message):
+        self.errors.append(error_message)
+
+    def print_summary(self):
+        print("\nSummary Report:")
+        print("--------------------------")
+        
+        print(f"Total files wiped: {len(self.wiped_files)}")
+        for file in self.wiped_files:
+            print(f"- {file}")
+
+        print(f"Total directories wiped: {len(self.wiped_directories)}")
+        for directory in self.wiped_directories:
+            print(f"- {directory}")
+
+        if self.errors:
+            print("\nErrors encountered:")
+            for error in self.errors:
+                print(f"- {error}")
+        else:
+            print("\nNo errors encountered.")
